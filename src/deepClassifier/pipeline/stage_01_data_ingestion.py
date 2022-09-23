f15445fd6a8b7d60457d6a479e1f0864d95a3a82
sathishr11 <mail2sathish11@gmail.com>
2022-09-23 18:26:51 +0530

    pipeline for data ingestion

---
+++ src/deepClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,26 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import DataIngestion
+from deepClassifier import logger
+from deepClassifier.exception import CustomException
+import sys
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+def main():
+    config = ConfigurationManager()
+    data_ingestion_config = config.get_data_ingestion_config()
+    data_ingestion = DataIngestion(config=data_ingestion_config)
+    data_ingestion.download_file()
+    data_ingestion.unzip_and_clean()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+    except Exception as e:
+        logger.exception(e)
+        raise CustomException(e, sys) from e
