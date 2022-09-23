3961e8a5783e23b6269a5cd4adfc283be46a58e7
sathishr11 <mail2sathish11@gmail.com>
2022-09-23 18:25:29 +0530

    constants

---
+++ src/deepClassifier/constants/__init__.py
@@ -0,0 +1,10 @@
+"""
+Constans for the project
+
+Contains the path of the config and params
+"""
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("configs/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
