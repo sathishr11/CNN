25fd089a54c4082c190493064fff5eae22682efa
sathishr11 <mail2sathish11@gmail.com>
2022-10-01 22:38:23 +0530

    Pipeline for training

---
+++ src/deepClassifier/pipeline/stage_03_training.py
@@ -0,0 +1,35 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import PrepareCallback
+from deepClassifier.components import Training
+from deepClassifier import logger
+from deepClassifier.exception import CustomException
+import sys
+
+
+STAGE_NAME = "Training with callbacks"
+
+
+def main():
+    config = ConfigurationManager()
+    prepare_callbacks_config = config.get_prepare_callback_config()
+    prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+    callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+    
+    training_config = config.get_training_config()
+    training = Training(config=training_config)
+    training.get_base_model()
+    training.train_valid_generator()
+    training.train(
+        callback_list=callback_list
+    )
+
+
+if __name__ == "__main__":
+    try:
+        logger.info("*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise CustomException(e, sys) from e
